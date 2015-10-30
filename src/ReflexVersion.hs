c2be64457997268d9df8e4b6fb298152e17fc78a
Doug Beardsley <mightybyte@gmail.com>
2015-10-30 16:41:32 -0400

    Update for reflex-0.3.1

--- src/ReflexVersion.hs
+++ src/ReflexVersion.hs
@@ -30,38 +30,12 @@ exec = runSpiderHost $ do
     resultEventHandle <- subscribeEvent resultEvent
 
     forM_ furEliseNotes $ \(hz, time) -> do
-      fire freqTriggerRef hz
+      fireEventRef freqTriggerRef hz
       let sampleRate = 44100
       let numSamples = floor (time * sampleRate)
       forM_ (replicate numSamples (1 / sampleRate)) $ \inputSample -> do
-        resultSample <- fireAndRead sampleTriggerRef inputSample resultEventHandle
+        resultSample <- fireEventRefAndRead sampleTriggerRef inputSample resultEventHandle
         maybe (return ()) (liftIO . hPutBuilder stdout . int16LE . toInt) resultSample
   where
     toInt = toPCM . (* 0.1)
-
-fire
-    :: (MonadReflexHost t m, MonadIO m)
-    => IORef (Maybe (EventTrigger t a))
-    -> a
-    -> m ()
-fire mtRef input = do
-    mt <- liftIO (readIORef mtRef)
-    maybe (return ()) (\trigger -> fireEvents [trigger :=> input]) mt
-
-fireAndRead
-    :: (MonadReflexHost t m, MonadIO m)
-    => IORef (Maybe (EventTrigger t a))
-    -> a
-    -> EventHandle t b
-    -> m (Maybe b)
-fireAndRead mtRef input e = do
-    mt <- liftIO (readIORef mtRef)
-    case mt of
-      Nothing -> return Nothing
-      Just trigger -> do
-          fireEventsAndRead [trigger :=> input] $ do
-              mAction <- readEvent e
-              case mAction of
-                Nothing -> return Nothing
-                Just action -> Just <$> action
 
